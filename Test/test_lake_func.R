8e9d773811f291bc2d1953a9fa7e129b10dec4dd
Alia Ajina <aliaajina@gmail.com>
2023-03-10 15:55:08 -0800

    test works woohoo

--- Test/test_lake_func.R
+++ Test/test_lake_func.R
@@ -1,3 +1,3 @@
 test_that("lake_equation_works", {
-  expect_equal(summary(lake_func(initial_level = 100000, precip_data = 10, outflow = 10, evap = 1), 99999, -1))
+  expect_equal(lake_func(initial_level = 100000, precip_data = 10, outflow = 10, evap = 1), list(99999,-1))
 })
