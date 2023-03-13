92793dfb8b91f922abfcae0ce449630a2de4a60d
Alia Ajina <aliaajina@gmail.com>
2023-03-13 09:56:06 -0700

    sourcing our function within the test

--- Test/test_lake_func.R
+++ Test/test_lake_func.R
@@ -1,4 +1,6 @@
 # Testing that our lake_func works with two different sets of data
+library(here)
+source(here("assignment_5_ajina_leiker", "R", "lake_equation.R"))
 
 test_that("lake_equation_works", {
   expect_equal(lake_func(initial_level = 100000, precip_data = 10, outflow = 10, evap = 1), list(99999,-1))
