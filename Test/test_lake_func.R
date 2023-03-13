e39f87cef1e71240c42cecdc99b7152ce9ae2037
Alia Ajina <aliaajina@gmail.com>
2023-03-13 10:51:01 -0700

    updating test issue

--- Test/test_lake_func.R
+++ Test/test_lake_func.R
@@ -1,7 +1,4 @@
 # Testing that our lake_func works with two different sets of data
-library(here)
-source(here("assignment_5_ajina_leiker", "R", "lake_equation.R"))
-
 test_that("lake_equation_works", {
   expect_equal(lake_func(initial_level = 100000, precip_data = 10, outflow = 10, evap = 1), list(99999,-1))
   expect_equal(lake_func(initial_level = 200, precip_data = 1, outflow = 1, evap = 1), list(199,-1))
